b9d61d66325fbb50eda4b301da04f27fb7386d9d
iamkorniichuk <iamkorniichuk@gmail.com>
2025-04-05 15:11:12 +0300

    feat: allow multiple-device login

--- web/web/settings.py
+++ web/web/settings.py
@@ -120,7 +120,5 @@ AUTH_USER_MODEL = "users.User"
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
     "TOKEN_OBTAIN_SERIALIZER": "authentication.serializers.UserTokenObtainPairSerializer",
 }
