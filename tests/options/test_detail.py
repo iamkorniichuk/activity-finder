3c68f14f254498fc4c37b7df98f9737e02abca74
iamkorniichuk <iamkorniichuk@gmail.com>
2025-07-03 13:54:47 +0300

    test: remove excessive tests

--- tests/options/test_detail.py
+++ tests/options/test_detail.py
@@ -1,6 +1,3 @@
-from uuid import uuid4
-
-
 class TestDetailOptions:
     url = "/options/"
 
@@ -13,15 +10,3 @@ class TestDetailOptions:
         assert response.status_code == 200
         data = response.json()
         assert isinstance(data, dict)
-
-    def test_remove_duplicate_patch(self, auth_client, option_data, create_option):
-        detail_url = self.build_url(create_option.json()["pk"])
-        data = {"name": f"Unique-Tier-{uuid4().hex[:8]}"}
-        response = auth_client.patch(detail_url, json=data)
-        assert response.status_code == 200
-
-        list_url = self.url
-        response = auth_client.post(list_url, json=option_data)
-        assert response.status_code == 201
-        data = response.json()
-        assert data["name"] == option_data["name"]
--- tests/options/test_list.py
+++ tests/options/test_list.py
@@ -7,10 +7,6 @@ class TestListOptions:
         data = response.json()["results"]
         assert isinstance(data, list)
 
-    def test_duplicate_name_post(self, auth_client, option_data, create_option):
-        response = auth_client.post(self.url, json=option_data)
-        assert response.status_code == 400
-
     def test_foreign_activity_post(self, auth_client1, option_data):
         response = auth_client1.post(self.url, json=option_data)
         assert response.status_code == 400
